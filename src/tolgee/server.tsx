21dd6f544b997e723ceea4eb1d6f50b17c03ae63
Tolgee Automation Machine <machine@tolgee.io>
2024-10-30 13:17:37 +0000

    v5.30.0 [skip ci] (published from monorepo)

--- src/tolgee/server.tsx
+++ src/tolgee/server.tsx
@@ -1,22 +1,20 @@
 import { getLocale } from 'next-intl/server';
 
-import { TolgeeBase, ALL_LOCALES, getStaticData } from './shared';
+import { TolgeeBase, ALL_LANGUAGES, getStaticData } from './shared';
 import { createServerInstance } from '@tolgee/react/server';
 
 export const { getTolgee, getTranslate, T } = createServerInstance({
   getLocale: getLocale,
-  createTolgee: async (locale) =>
+  createTolgee: async (language) =>
     TolgeeBase().init({
-      // including all locales
+      // including all languages
       // on server we are not concerned about bundle size
-      staticData: await getStaticData(ALL_LOCALES),
+      staticData: await getStaticData(ALL_LANGUAGES),
       observerOptions: {
         fullKeyEncode: true,
       },
-      language: locale,
-      fetch: async (input, init) => {
-        const data = await fetch(input, { ...init, next: { revalidate: 0 } });
-        return data;
-      },
+      language,
+      fetch: async (input, init) =>
+        fetch(input, { ...init, next: { revalidate: 0 } }),
     }),
 });
